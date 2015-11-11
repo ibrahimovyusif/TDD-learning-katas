17facc725474c725605699c0323249876590d018
ibrahimovyusif <yusifibrahimov@hotmail.com>
2015-11-11 18:04:50 +0400

    ferid

--- controllers/tdd-add.js
+++ controllers/tdd-add.js
@@ -1,49 +1,64 @@
 'use strict';
 
+var util = require('util');
+
 function add(input){
 
 	var sumOfInputs = 0;
+	var delimiter = ',';
 	
-
-
-		var replacedInput = input.replace(/\n|;|\/|[a-zA-Z\s]/g, ",");
-
-		var inputNumbers = replacedInput.split(",");
-		var negatives = [];
-
-		for(var i = 0; i < inputNumbers.length; i++){
-			
-			if(isNegative(inputNumbers[i])){
-				negatives.push(inputNumbers[i]);
-			}
-				
-			sumOfInputs += Number(inputNumbers[i]);
-		}
-
-		if(negatives.length !== 0 ){
-
-			throw new Error("negative numbers are not allowed: " + negatives);			
-		}
+	var inputNumbers;
+	var negatives = [];
+	
+	//
+	if(input[0] === '/' && input[1] === '/'){
 		
-		return sumOfInputs;
-	
-	
+		delimiter = input[3];
+	}
+
+	inputNumbers = splitToNumber(input, delimiter);
+
+	inputNumbers.forEach(function (num) {
+		if(num >=1000){
+			num = 0;
+		}
+
+		if(isNegative(num)){
+			negatives.push(num);
+		}
+
+		sumOfInputs += Number(num);
+	});
+
+	if(negatives.length !== 0 ){
+
+		throw new Error("negative numbers are not allowed: " + negatives);			
+	}
 
 
+
+	return sumOfInputs;
 }
 
+function splitToNumber(stringOfNumbers, delimiter) {
+
+	var regexString = ['\n|\/|', delimiter].join(''); 
+	var regex = new RegExp(regexString, "g");
+	var replacedInput = stringOfNumbers.replace(regex, ',');
+
+	return replacedInput.split(',');
+}
 
 function isNegative(num){
+
 	if(num < 0){
 		return true;
 	}
-
 }
 
 function isNumber(num) {
+
 	return !(isNaN(num));
 };
-
-
 
 module.exports = add;
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,12 +1,13 @@
 'use strict';
 var chai = require('chai');
 
+
 var assert = chai.assert;
 chai.expect;
 
 var add = require('../controllers/tdd-add');
 
-describe.only('testing add method', function () {
+describe('testing add method', function () {
 	
 	it ('should return 0 when input is empty', function () {
 
@@ -50,20 +51,18 @@ describe.only('testing add method', function () {
 
 	it ('should return the sum of the inputs if there are two inputs. example input(\'5\',\'2\') result :7 ', function () {
 
-	// setup
+		// setup
 		var input = '5,2';
 
 		// expectation
-
 		var expected = 7;
 
 		// action / execution
-
 		var result = add(input);
 
 		// validation
-
 		assert.equal(result,expected);
+
 		// teardown
 
 	});
@@ -71,10 +70,10 @@ describe.only('testing add method', function () {
 	it ('should return the sum of inputs when the amount of inputs is unknown', function () {
 
 		// setup
-		var input ='1,2,3,4,5,6,7,8,9,11'; 
+		var input ='1,2,3,4,5,6,7,8,9,12'; 
 		
 		// expectation
-		var expected = 56;
+		var expected = 57;
 		
 		// action / execution 
 		var result = add(input);
@@ -109,7 +108,7 @@ describe.only('testing add method', function () {
 	it ('should return the sum of inputs when the amount of inputs is unknown and there are different delimiters between numbers', function () {
 
 		// setup
-		var input ='//shgfj;1,2\n3,4,5,6,7\n8,9\n11\n'; 
+		var input ='//;\n1;2;3;4;5;6;7;8;9;11'; 
 		
 		// expectation
 		var expected = 56;
@@ -128,7 +127,7 @@ describe.only('testing add method', function () {
 	it ('should return an exception when a negative number is given ', function () {
 
 		// setup
-		var input ='//shgfj;1,-2\n3,-4,5,6,7\n8,9\n11\n'; 
+		var input ='//;1,-2\n3,-4,5,6,7\n8,9\n11\n'; 
 		
 		// expectation
 		var expected = 'negative numbers are not allowed: -2,-4';
@@ -136,7 +135,7 @@ describe.only('testing add method', function () {
 		// action / execution 
 		try {
 			var result = add(input);
-			assert.fail();
+			assert.fail(null, null, 'Negatives numbers error not thrown');
 		}
 		catch (err) {
 
@@ -149,5 +148,61 @@ describe.only('testing add method', function () {
 
 
 	});	
+
+	it ('should ignore the numbers which are greater than 1000. example: addimg 1002 to 2 will give 2', function () {
+
+		// setup
+		var input ='2,3,1002,1004,2,250,3'; 
+		
+		// expectation
+		var expected = '260';
+		
+		// action / execution 
+		var result = add(input);
+		
+		// validation
+		assert.equal(result,expected);
+		
+		// teardown
+
+	});	
+
+	it.only ('should return the sum of inputs when the amount of inputs is unknown and there are different lengths of delimiters between numbers', function () {
+
+		// setup
+		var input ='//;;;\n1;;;2;;;3;;;4;;;5;;;6;;;7;;;8;;;9;;;11'; 
+		
+		// expectation
+		var expected = 56;
+		
+		// action / execution 
+		var result = add(input);
+		
+		// validation
+		assert.equal(result,expected);
+		
+		// teardown
+
+
+	});	
+
+	// it ('should return the sum of inputs when the amount of inputs is unknown and there are different kind of delimiters between numbers', function () {
+
+	// 	// setup
+	// 	var input ='//;%\n1;2%3;4%5;6%7;8%9;11'; 
+		
+	// 	// expectation
+	// 	var expected = 56;
+		
+	// 	// action / execution 
+	// 	var result = add(input);
+		
+	// 	// validation
+	// 	assert.equal(result,expected);
+		
+	// 	// teardown
+
+
+	// });	
 
 });
